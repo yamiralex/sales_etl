473dcac966f08cea9d1bdfba449e97b9fce6c268
yamir.minaya <yamirminaya@cnx.com>
2026-06-04 19:38:16 -0300

    update repository

--- sales_etl_platform/dbt/target/compiled/sales_etl/tests/unique_transaction.sql
+++
@@ -1,9 +0,0 @@
-select
-    store_token,
-    transaction_id,
-    count(*)
-from SALES_DB.SILVER.silver_sales
-group by
-    store_token,
-    transaction_id
-having count(*) > 1
--- sales_etl_platform/dbt/target/compiled/sales_etl/tests/valid_store_token.sql
+++
@@ -1,6 +0,0 @@
-select
-    s.store_token
-from SALES_DB.SILVER.silver_sales s
-left join SALES_DB.SILVER.silver_stores st
-    on s.store_token = st.store_token
-where st.store_token is null
